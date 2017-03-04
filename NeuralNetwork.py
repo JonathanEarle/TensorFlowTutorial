7e0f28d33d21e70a4d6ff521eeb79f2863df7cf7
JonathanEarle <jonathan.john.enock.earle@gmail.com>
2017-03-04 16:22:32 -0400

    Added more documentation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cv2
 from tensorflow.examples.tutorials.mnist import input_data
+
+#One hot encoding means [0,0,0,1,0,0,0,0,0,0,0] represents 3 [0,0,0,0,1,0,0,0,0,0,0] represents 4 and so forth
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
 
 #Input and Output placeholders
-x=tf.placeholder('float',[None,28*28])
-y=tf.placeholder('float',[None,10])
+x=tf.placeholder('float',[None,28*28])#28*28 size images are the inputs
+y=tf.placeholder('float',[None,10])#An array of size 10 stores the one hot encoded results
 
+#Path to save the model to
 modelPath='/tmp/model.ckpt'
 
-#design network
+#Model of the neural net
 def neuralNetwork(x):
-	#Variable weights and biases
+	#Variable weights and biases, randomly initalized
 	weights={'hid1':tf.Variable(tf.random_normal([28*28,500])),
 			 'hid2':tf.Variable(tf.random_normal([500,500])),
 			 'out':tf.Variable(tf.random_normal([500,10])),}
@@ -21,53 +24,66 @@ def neuralNetwork(x):
 			 'b2':tf.Variable(tf.random_normal([500])),
 			 'out':tf.Variable(tf.random_normal([10])),}
 
+	#A single layer multiplies its input by the weights and adds the bias
+	#relu (Rectified linear unit) is our activation function
 	layer1=tf.add(tf.matmul(x,weights['hid1']),biases['b1'])
 	layer1=tf.nn.relu(layer1)
 
+	#Each layer takes in the output from the previous layer and applies the operations
 	layer2=tf.add(tf.matmul(layer1,weights['hid2']),biases['b2'])
 	layer2=tf.nn.relu(layer2)
 
 	output=tf.add(tf.matmul(layer2,weights['out']),biases['out'])
 	return output
 
-#train model
+#Training the network on data (x),segmenting the data in 100 batches and repeating this for 10 epochs
 def trainModel(x,learnRate=0.001,batch=100,epochs=10):
 	prediction=neuralNetwork(x)
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))#Activation Fn
-	optimizer = tf.train.AdamOptimizer(learning_rate=learnRate).minimize(cost)#GD
+
+	#Activation Function applied to the output layer, cross entropy is our cost function
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))
+
+	#Adam Optimizer used for cost minimization
+	optimizer = tf.train.AdamOptimizer(learning_rate=learnRate).minimize(cost)
 	
 	init=tf.global_variables_initializer()
 
+	#Saver to save our model
 	saver=tf.train.Saver()
 	with tf.Session() as sess:
 		sess.run(init)	
 
 		#Training
 		for epoch in range(epochs):
-			loss=0
-
+			loss=0#stores our total loss at each epoch
 			totalBatch=int(mnist.train.num_examples/batch)
 			for i in range(totalBatch):
 				batch_x,batch_y = mnist.train.next_batch(batch)
 
+				#Actually running the process with a batch of data and store the loss (_ results from optimizer)
 				_,c=sess.run([optimizer,cost],feed_dict={x:batch_x, y:batch_y})
 				loss+=c
 			print("Epoch ",(epoch+1)," Loss: ",loss)
 
 		#Evaluate accuracy and save
+		#argmax returns index of largest value in the output (highest confidence score) along axis 1 (each row)
 		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
 		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 		print("Accuracy: ",accuracy.eval({x: mnist.test.images[:256],y: mnist.test.labels[:256]}))
+
+		#Aave the session, with the trained weights for reuse
 		saver.save(sess,modelPath)
 
-	#test model
+	#Open a new session and attempt to restore the model
 	with tf.Session() as sess:
 		sess.run(init)	
 		saver.restore(sess, modelPath)
 
-		#Evaluate accuracy
-		print(tf.argmax(prediction,1).eval({x: mnist.test.images[:1]}))
-		print(tf.argmax(mnist.test.labels[:1],1)).eval()
+		#See the result given by the model on one test image
+		print(tf.argmax(prediction,1).eval({x: mnist.test.images[:1]}))#Neural network answer
+		print(tf.argmax(mnist.test.labels[:1],1)).eval()#Actual answer
+		
+		#Display the image
 		img=np.array(mnist.test.images[:1]).reshape(28,28)
 		cv2.imshow("Result", img)
 		cv2.waitKey(0)
--- Tutorial.py
+++ Tutorial.py
@@ -6,23 +6,32 @@ import tensorflow as tf
 #TF represents computations
 x=tf.constant(5)
 
+#These variables represent various computations
 y=tf.Variable(x*2)
 z=tf.Variable(tf.mul(x,2))
 
 y2=tf.Variable(x+2)
 z2=tf.Variable(tf.add(x,2))
 
-print(x)
-print(y)
+#x and y are tensors print they need to be run in a session
+print(x)#Outputs: Tensor("Const:0", shape=(), dtype=int32)
+print(y)#Outputs: Tensor("Variable/read:0", shape=(), dtype=int32)
 
+
+#With statement used to open a session, it is automatically closed when finished
 with tf.Session() as sess:
+	#This statement initalizes all our variables in the current session
 	sess.run(tf.global_variables_initializer())
-	print(sess.run(y))
-	print(z.eval())
 
-	print(sess.run(y2))
-	print(sess.run(z2))
+	#Run a particular computation using eval or run
+	print(sess.run(y))#Outputs: 10
+	print(z.eval())#Outputs: 10
 
+	#An important difference between eval and run is that run can work with multiple inputs
+	print(sess.run([y2,z2]))#Outputs: [7, 7] 
+	print(sess.run(z2))#Outputs: 7
+
+#Without with the session must be explicitly closed
 sess=tf.Session()
 sess.run(tf.global_variables_initializer())
 print(sess.run(y))
@@ -30,7 +39,7 @@ print(sess.run(z))
 
 print(sess.run(y2))
 print(sess.run(z2))
-sess.close()
+sess.close()#Close session when finished
 
 #---------------------------------
 #A placeholder is a variable that will be assigned data to later
@@ -40,19 +49,19 @@ x=tf.placeholder("float",3)
 y=5*(x*x)-(3*x)+15
 
 with tf.Session() as sess:
-	print(sess.run(y,feed_dict=({x:[0,1,2]})))
+	print(sess.run(y,feed_dict=({x:[0,1,2]})))#Outputs: [ 15.  17.  29.]
 
 #---------------------------------
-#If we don't know the size
-
+#If we don't know the size being put in the placeholder, None is used
 x=tf.placeholder("float",[None,3])
 y=5*(x*x)-(3*x)+15
 
 with tf.Session() as session:
+	#The input data can now consist of many arrays or size 3 rather than just one
     x_data = [[0, 1, 2],
               [3, 4, 5],]
     result = session.run(y, feed_dict={x: x_data})
-    print(result)
+    print(result)#Outputs: [[  15.   17.   29.] [  51.   83.  125.]]
 
 #---------------------------------
 #Simple addition
@@ -65,12 +74,16 @@ init=tf.global_variables_initializer()
 
 with tf.Session() as sess:
 	sess.run(init)
-	result=sess.run(add,feed_dict={a:2,b:3})
+	result=sess.run(add,feed_dict={a:2,b:3})#Outputs: 5
 	print(result)
 
 #---------------------------------
-#Simple addition tensorboard
+#Simple addition with tensorboard
+#Tensorboard allows us to visualize how the data is 
+#flowing in the computational graph through each mathematical operation 
 
+#Adding name scopes allows us to identify what operation tf is refering to
+#Names are defaulted to ones which aren't easy to identify
 with tf.name_scope("A"):
 	a=tf.placeholder(tf.int16)
 
@@ -80,20 +93,26 @@ with tf.name_scope("B"):
 with tf.name_scope("ADDER"):
 	add=tf.add(a,b)
 
+#Scalars graph the progress of computations
 tf.summary.scalar("Result", add)
-merged_summary_op = tf.summary.merge_all()
+
+#All the summaries are merged
+mergedSummary = tf.summary.merge_all()
 
 init=tf.global_variables_initializer()
 
 with tf.Session() as sess:
 	sess.run(init)
 
-	summary_writer = tf.summary.FileWriter("/tmp/tensorflow_logs/example", graph=tf.get_default_graph())
+	#Writer to write our graph to a temp location
+	summaryWriter = tf.summary.FileWriter("/tmp/tensorflow_logs/example", graph=tf.get_default_graph())
 
-	result,summary=sess.run([add,merged_summary_op],feed_dict={a:2,b:3})
+	result,summary=sess.run([add,mergedSummary],feed_dict={a:2,b:3})
 
-	summary_writer.add_summary(summary)
+	#Summary is added to our temp location
+	summaryWriter.add_summary(summary)
 
+	#Run the following command in console after running script and put the link returned into a browser
 	#tensorboard --logdir=/tmp/tensorflow_logs
 
-#Saving models covered in MLP
+#Saving models covered in NeuralNetwork.py
